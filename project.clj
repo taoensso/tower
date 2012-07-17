ca974ff2add679e17a1ca5329e20c6b61938c024
Peter Taoussanis <ptaoussanis@gmail.com>
2012-07-17 22:25:39 +0700

    Clean up testing config.

--- project.clj
+++ project.clj
@@ -4,16 +4,17 @@
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.taoensso/timbre "0.7.0"]]
-  :profiles {:1.3   {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5   {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :dev   {:dependencies []}}
+  :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :dev  {:dependencies [[ring/ring-core  "1.1.0"]]}
+             :test {:dependencies [[ring/ring-core  "1.1.0"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases {"all" ["with-profile" "1.3:1.4:1.5"]}
+  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
