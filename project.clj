78f859523ddf23e32cbb800fa4ca09f93fae6f10
Peter Taoussanis <ptaoussanis@gmail.com>
2013-05-18 16:55:20 +0700

    Bump Clojure 1.5 test dependency (1.5.1)

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.taoensso/timbre     "1.5.2"]]
   :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies [[ring/ring-core      "1.1.0"]]}
              :test {:dependencies [[ring/ring-core      "1.1.0"]]}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
