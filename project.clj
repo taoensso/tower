14dabe2d9899169b30565fcf1cec7c7547439a20
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-01 19:59:55 +0700

    Update project.clj (NB: drop official Clojure 1.3 support)

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject com.taoensso/tower "1.6.0"
-  :description "Clojure internationalization & translation library"
+  :description "Clojure i18n & L10n library"
   :url "https://github.com/ptaoussanis/tower"
-  :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure     "1.3.0"]
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure     "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]
-                 [com.taoensso/timbre     "1.5.2"]]
-  :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+                 [com.taoensso/timbre     "1.6.0"]]
+  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies [[ring/ring-core      "1.1.0"]]}
              :test {:dependencies [[ring/ring-core      "1.1.0"]]}}
-  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+  :aliases {"test-all" ["with-profile" "test,1.4:test,1.5" "test"]}
+  :plugins [[codox "0.6.4"]]
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
