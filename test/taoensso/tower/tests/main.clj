e6d33d4e9dd763423e6aa8893287ed145fd28576
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-26 14:49:34 +0700

    Add `before-run`, `after-run` Expectations fns

--- test/taoensso/tower/tests/main.clj
+++ test/taoensso/tower/tests/main.clj
@@ -2,4 +2,7 @@
   (:require [expectations   :as test  :refer :all]
             [taoensso.tower :as tower :refer ()]))
 
+(defn- before-run {:expectations-options :before-run} [])
+(defn- after-run  {:expectations-options :after-run}  [])
+
 (expect true) ; TODO Add tests (PRs welcome!)
