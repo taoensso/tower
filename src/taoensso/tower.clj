10d69ef573edcd1de3ae45eab677589c3f539272
Peter Taoussanis <ptaoussanis@gmail.com>
2013-07-12 12:56:37 +0700

    Rename (deprecated) `t'` -> `t-`

--- src/taoensso/tower.clj
+++ src/taoensso/tower.clj
@@ -531,4 +531,4 @@
   `(with-tscope ~translation-scope ~@body))
 
 ;; BREAKS v1 due to unavoidable name clash
-(def t' #(apply t (or *locale* :jvm-default) @config %&))
+(def t- #(apply t (or *locale* :jvm-default) @config %&))
