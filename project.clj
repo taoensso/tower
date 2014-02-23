28988b97d9e3630e80219a86fff1302231fb40c8
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-23 22:05:47 +0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,33 +1,69 @@
 (defproject com.taoensso/tower "2.1.0-SNAPSHOT"
+  :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure i18n & L10n library"
   :url "https://github.com/ptaoussanis/tower"
   :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure     "1.4.0"]
-                 [org.clojure/tools.macro "0.1.5"]
-                 [markdown-clj            "0.9.35"]
-                 [com.taoensso/timbre     "2.7.1"]]
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]}
-             :dev  {:dependencies [[org.clojure/clojurescript "0.0-2080"]]
-                    :cljsbuild {:builds
-                                {:dev {:source-paths ["src/cljs"]
-                                       :compiler {}}}}}
-             :test {:dependencies [[expectations   "1.4.56"]
-                                   [ring/ring-core "1.2.1"]]}}
-  :aliases {"test-all"    ["with-profile" "+test,+1.4:+test,+1.5:+test,+1.6" "expectations"]
-            "test-auto"   ["with-profile" "+test" "autoexpect"]
-            "start-dev"   ["with-profile" "+dev,+test,+bench" "repl" ":headless"]
-            "codox"       ["with-profile" "+test" "doc"]}
-  :plugins [[lein-cljsbuild    "1.0.0"]
-            [lein-expectations "0.0.8"]
-            [lein-autoexpect   "1.0"]
-            [lein-ancient      "0.5.4"]
-            [codox             "0.6.6"]]
-  :min-lein-version "2.0.0"
-  :global-vars {*warn-on-reflection* true}
-  :source-paths ["src/clj" "src/cljs"]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Same as Clojure"}
+  :min-lein-version "2.3.3"
+  :global-vars {*warn-on-reflection* true
+                *assert* true}
+  :dependencies
+  [[org.clojure/clojure "1.4.0"]
+   [markdown-clj        "0.9.35"]
+   [com.taoensso/encore "0.8.0"]
+   [com.taoensso/timbre "3.1.0"]]
+
+  :cljsbuild {:builds []}
+  :test-paths ["test" "src"]
+  :profiles
+  {:build {:hooks ^:replace []} ; Workaround to avoid :dev hooks during deploy
+   :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
+   :test {:dependencies [[expectations            "1.4.56"]
+                         [reiddraper/simple-check "0.5.6"]
+                         [ring/ring-core          "1.2.1"]]
+          :plugins [[lein-expectations "0.0.8"]
+                    [lein-autoexpect   "1.2.2"]]}
+   :dev
+   [:1.6 :test
+    {:jvm-opts ^:replace ["-server"]
+     :hooks [cljx.hooks leiningen.cljsbuild]
+     :dependencies
+     [[org.clojure/clojurescript "0.0-2173" :scope "provided"]]
+     :plugins
+     [[lein-cljsbuild                  "1.0.2"]
+      [com.cemerick/clojurescript.test "0.2.2"]
+      [com.keminglabs/cljx             "0.3.2"] ; Must precede Austin!
+      [com.cemerick/austin             "0.1.4"]]
+
+     :cljx
+     {:builds
+      [{:source-paths ["src" "test"] :rules :clj  :output-path "target/classes"}
+       {:source-paths ["src" "test"] :rules :cljs :output-path "target/classes"}]}
+
+     :cljsbuild
+     {:test-commands {"node"    ["node" :node-runner "target/main.js"]
+                      "phantom" ["phantomjs" :runner "target/main.js"]}
+      :builds ; Compiled in parallel
+      [{:id :main
+        :source-paths ["src" "test" "target/classes"]
+        :compiler     {:output-to "target/main.js"
+                       :optimizations :advanced
+                       :pretty-print false}}]}}]}
+
+  :plugins [[lein-ancient "0.5.4"]
+            [codox        "0.6.7"]]
+
+  :codox {:sources ["target/classes"]} ; For use with cljx
+  :aliases
+  {"test-all"   ["with-profile" "+test:+1.6,+test" "expectations"]
+   "test-auto"  ["with-profile" "+test" "autoexpect"]
+   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   "codox"      ["with-profile" "+test" "doc"]
+   "deploy-lib" ["with-profile" "+dev,+build" "do" "deploy" "clojars," "install"]}
+
   :repositories
   {"sonatype"
    {:url "http://oss.sonatype.org/content/repositories/releases"
