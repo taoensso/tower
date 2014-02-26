4ef8fb3d4eba7392a01a2bcd93fcd734d9da9fc2
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-26 16:56:54 +0700

    Fix project.clj to prevent unnecessary downstream deps

--- project.clj
+++ project.clj
@@ -11,14 +11,13 @@
                 *assert* true}
   :dependencies
   [[org.clojure/clojure "1.4.0"]
-   [com.taoensso/encore "0.8.0"]
-   [com.taoensso/timbre "3.1.0"]
+   [com.taoensso/encore "0.9.2"]
+   [com.taoensso/timbre "3.1.1"]
    [markdown-clj        "0.9.35"]]
 
-  :cljsbuild {:builds []}
   :test-paths ["test" "src"]
   :profiles
-  {:build {:hooks ^:replace []} ; Workaround to avoid :dev hooks during deploy
+  {;; :default [:base :system :user :provided :dev]
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
    :test {:dependencies [[expectations            "1.4.56"]
@@ -27,11 +26,11 @@
                           :exclusions [org.clojure/tools.reader]]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
+   :dev* [:dev {:jvm-opts ^:replace ["-server"]
+                :hooks [cljx.hooks leiningen.cljsbuild]}]
    :dev
    [:1.6 :test
-    {:jvm-opts ^:replace ["-server"]
-     :hooks [cljx.hooks leiningen.cljsbuild]
-     :dependencies
+    {:dependencies
      [[org.clojure/clojurescript "0.0-2173"]]
      :plugins
      [[lein-cljsbuild                  "1.0.2"]
@@ -59,11 +58,12 @@
 
   :codox {:sources ["target/classes"]} ; For use with cljx
   :aliases
-  {"test-all"   ["with-profile" "+test:+1.6,+test" "expectations"]
+  {"test-all"   ["with-profile" "default:+1.5:+1.6" "expectations"]
+   ;; "test-all"   ["with-profile" "default:+1.6" "expectations"]
    "test-auto"  ["with-profile" "+test" "autoexpect"]
-   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
-   "codox"      ["with-profile" "+test" "doc"]
-   "deploy-lib" ["with-profile" "+dev,+build" "do" "deploy" "clojars," "install"]}
+   "build-once" ["do" "cljx" "once," "cljsbuild" "once"]
+   "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]
+   "start-dev"  ["with-profile" "+dev*" "repl" ":headless"]}
 
   :repositories
   {"sonatype"
